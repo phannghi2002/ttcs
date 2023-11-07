ea55b34ed66e347fcbee6e65b643d0d2c3e9e143
Rebornl2002 <146551221+Rebornl2002@users.noreply.github.com>
2023-11-07 18:47:00 +0700

    Header

---
+++ frontend/src/component/DefaultPage/Header/Header.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import Navbar from './Navbar';
+
+const cx = classNames.bind(styles);
+
+function Header() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header-container')}>
+                <div className={cx('header-logo')}>
+                    <img className={cx('logo-img')} alt="logo" src="https://res.flynow.vn/logoflynow.png" />
+                    <h1 className={cx('header-text')}>Vé máy bay, đặt mua vé máy bay tại đại lý vé máy bay Flynow</h1>
+                </div>
+                <span className={cx('hotline')}>
+                    Hotline: 1900 6432
+                    <span className={cx('space')}></span>
+                    (024) 7300 1133 - (028) 7300 1133
+                </span>
+            </div>
+            <Navbar />
+        </div>
+    );
+}
+
+export default Header;
