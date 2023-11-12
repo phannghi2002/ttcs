d470c0728a8475fccefb174a57d38dfff49f8375
Phan Nghi <phannghi2002nk@gmail.com>
2023-11-12 21:04:11 +0700

    send email success

--- backend/models/emailer.js
+++ backend/models/emailer.js
@@ -7,7 +7,7 @@
 // // Những thông tin dưới đây các bạn có thể ném nó vào biến môi trường env nhé.
 // // Vì để demo nên mình để các biến const ở đây.
 // const adminEmail = "phannghi2002nk@gmail.com";
-// const adminPassword = "Phandangnghi2002@1";
+// const adminPassword = "co cai nit doi mat khau";
 // // Mình sử dụng host của google - gmail
 // const mailHost = "smtp.gmail.com";
 // // 587 là một cổng tiêu chuẩn và phổ biến trong giao thức SMTP
