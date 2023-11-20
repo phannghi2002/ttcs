7fbdf0fbdf5b7de81c33104e8e46ee9ba3deeb31
Rebornl2002 <146551221+Rebornl2002@users.noreply.github.com>
2023-11-20 23:17:48 +0700

    footer

--- frontend/src/component/DefaultPage/Header/Navbar/Navbar.js
+++ frontend/src/component/DefaultPage/Header/Navbar/Navbar.js
@@ -1,31 +1,33 @@
-import classNames from "classnames/bind";
-import styles from "./Navbar.module.scss";
-import { Link } from "react-router-dom";
+import classNames from 'classnames/bind';
+import styles from './Navbar.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function Navbar() {
-  const handleClick = () => {
-    localStorage.clear();
-  };
-  return (
-    <div className={cx("wrapper")}>
-      <div className={cx("header-container")}>
-        <ul className={cx("navbar")}>
-          <Link to="/" className={cx("home")} onClick={handleClick}>
-            <li className={cx("navbar-item")}>Trang chủ</li>
-          </Link>
+    const handleClick = () => {
+        localStorage.clear();
+    };
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header-container')}>
+                <ul className={cx('navbar')}>
+                    <Link to="/" className={cx('home')} onClick={handleClick}>
+                        <li className={cx('navbar-item')}>Trang chủ</li>
+                    </Link>
 
-          <Link to="/myFlight" className={cx("home")}>
-            <li className={cx("navbar-item")}>Tra cứu chuyến bay của tôi</li>
-          </Link>
+                    <Link to="/myFlight" className={cx('home')}>
+                        <li className={cx('navbar-item')}>Tra cứu chuyến bay của tôi</li>
+                    </Link>
 
-          <li className={cx("navbar-item")}>Liên hệ</li>
-        </ul>
-        <span className={cx("login")}>Đăng nhập</span>
-      </div>
-    </div>
-  );
+                    <Link to="/contact" className={cx('home')}>
+                        <li className={cx('navbar-item')}>Liên hệ</li>
+                    </Link>
+                </ul>
+                <span className={cx('login')}>Đăng nhập</span>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
